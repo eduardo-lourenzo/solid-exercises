6d373ac928b1deed38353796c44906bc886bf55f
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-06 15:51:02 -0300

    feat(Subtract): implement operator method

--- src/ocp_open_closed_principle/Subtract.java
+++ src/ocp_open_closed_principle/Subtract.java
@@ -1,4 +1,8 @@
 package src.ocp_open_closed_principle;
 
-public class Subtract {
+public class Subtract implements Operation {
+    @Override
+    public int operator(int leftHandSide, int rightHandSide) {
+        return leftHandSide - rightHandSide;
+    }
 }
