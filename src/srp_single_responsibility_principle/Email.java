67cfaa9eb8e59ac4171a1c56690334aa6b293ed5
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-06 15:22:11 -0300

    feat(Main): add objects in Main method

--- src/srp_single_responsibility_principle/Email.java
+++ src/srp_single_responsibility_principle/Email.java
@@ -3,6 +3,9 @@ package src.srp_single_responsibility_principle;
 public class Email {
 
     public void sendEmail(String recipientEmail, String message) {
-        System.out.println("Enviando o "+ message + " para " + recipientEmail + "...");
+        System.out.println("E-mail enviando!\n"
+                + "Destinatário: " + recipientEmail + "\n"
+                + "Mensagem    : " + message + "\n"
+        );
     }
 }
--- src/srp_single_responsibility_principle/Main.java
+++ src/srp_single_responsibility_principle/Main.java
@@ -2,6 +2,10 @@ package src.srp_single_responsibility_principle;
 
 public class Main {
     public static void main(String[] args) {
-
+        Report report = new Report();
+        Email email = new Email();
+        String emailAddress = "fulano.Silva@zmail.com";
+        
+        email.sendEmail(emailAddress, report.generateReport());
     }
 }
