dd9ed7aa2e7d675437f1513650e81a1cb84af97a
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-06 16:17:17 -0300

    Style(Calculator): add Calculator method

--- src/ocp_open_closed_principle/Calculator.java
+++ src/ocp_open_closed_principle/Calculator.java
@@ -1,4 +1,14 @@
 package src.ocp_open_closed_principle;
 
 public class Calculator {
+    private Operation operation;
+
+    public int calculate(int leftHandSide, int rightHandSide, String operator) {
+        switch (operator) {
+            case "soma" -> operation = new Add();
+            case "subtração" -> operation = new Subtract();
+            case "multiplicação" -> operation = new Multiply();
+        }
+        return operation.operate(leftHandSide, rightHandSide);
+    }
 }
