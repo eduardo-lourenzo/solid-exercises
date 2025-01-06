899100f056d105e395531fa2b69735ecf2cd0d54
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-06 15:04:54 -0300

    feat(Email): add sendEmail method

--- src/srp_single_responsibility_principle/Email.java
+++ src/srp_single_responsibility_principle/Email.java
@@ -1,4 +1,8 @@
 package src.srp_single_responsibility_principle;
 
 public class Email {
+
+    public void sendEmail(String recipientEmail, String message) {
+        System.out.println("Enviando o "+ message + " para " + recipientEmail + "...");
+    }
 }
