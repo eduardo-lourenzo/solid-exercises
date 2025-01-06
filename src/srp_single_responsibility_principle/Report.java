72443a2aaa44af162a631f05b8a66bbb34126e66
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-06 14:53:46 -0300

    feat(Report): add generateReport method

--- src/srp_single_responsibility_principle/Report.java
+++ src/srp_single_responsibility_principle/Report.java
@@ -1,4 +1,14 @@
 package src.srp_single_responsibility_principle;
 
 public class Report {
+    private final double total;
+
+    public Report() {
+        total = 987.12;
+    }
+
+    public String generateReport() {
+        System.out.println("Gerando relatório financeiro...");
+        return "Total do Relatório Financeiro: " + total;
+    }
 }
