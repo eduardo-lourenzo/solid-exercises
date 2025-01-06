7370f035be1259ffc32fe8747d6ff42171d6d459
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-06 16:27:16 -0300

    feat(Main): implement main method

--- src/ocp_open_closed_principle/Main.java
+++ src/ocp_open_closed_principle/Main.java
@@ -1,4 +1,19 @@
 package src.ocp_open_closed_principle;
 
 public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println("soma: "
+                + calculator.calculate(50,50, "soma")
+        );
+
+        System.out.println("subtração: "
+                + calculator.calculate(14,1, "subtração")
+        );
+
+        System.out.println("multiplicação: "
+                + calculator.calculate(7,6, "multiplicação")
+        );
+    }
 }
