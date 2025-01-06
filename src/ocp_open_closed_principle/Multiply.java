e7b50826d8cff26bf16df71cf560a45810ff1b73
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-06 15:54:32 -0300

    feat(Multiply): implement operator method

--- src/ocp_open_closed_principle/Multiply.java
+++ src/ocp_open_closed_principle/Multiply.java
@@ -1,4 +1,8 @@
 package src.ocp_open_closed_principle;
 
-public class Multiply {
+public class Multiply implements Operation{
+    @Override
+    public int operator(int leftHandSide, int rightHandSide) {
+        return leftHandSide * rightHandSide;
+    }
 }
