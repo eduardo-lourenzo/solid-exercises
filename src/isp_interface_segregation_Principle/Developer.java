65ba1f9b216cd75686c956082b84b869dad4270a
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-07 15:31:13 -0300

    feat(ISP): create Developer class

---
+++ src/isp_interface_segregation_Principle/Developer.java
@@ -0,0 +1,8 @@
+package src.isp_interface_segregation_Principle;
+
+public class Developer implements Programmer {
+    @Override
+    public void program() {
+        System.out.println("Programando...");
+    }
+}
