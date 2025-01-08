4bff5fce0ecf57ef9c9b9218974b3945327147c2
Eduardo Lourenzo <eduardo.lourenzo@zup.com.br>
2025-01-08 11:52:16 -0300

    feat(DIP): create Computer class

---
+++ src/dip_dependency_inversion_principle/Computer.java
@@ -0,0 +1,21 @@
+package src.dip_dependency_inversion_principle;
+
+import java.util.ArrayList;
+
+public class Computer {
+    ArrayList<Device> devices;
+
+    public Computer() {
+        devices = new Devices();
+    }
+
+    public void setDevices(Device device) {
+        devices.add(device);
+    }
+
+    public void useAllDevices() {
+        for (Device device : devices) {
+            device.use();
+        }
+    }
+}
